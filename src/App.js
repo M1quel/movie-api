c34c8926158e2645e228684af80ef3fe6034b3bf
M1quel <5760459@rts-365.dk>
2021-08-16 11:26:03 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -3,26 +3,18 @@ import { useState } from 'react';
 import './App.scss';
 import Moviegrid from './components/movieGrid/MovieGrid';
 import Searchbar from "./components/searchbar/Searchbar";
+import displayNotification from './components/helpers/displayNotification';
 import Singlemovie from './components/singleMovie/Singlemovie';
 
 function App() {
   var [movies, setMovies] = useState([])
-  
+
   Notification.requestPermission(function(status) {
     console.log("Notification permission status: ", status)
   })
-  // function displayNotification () {
-  //   console.log("den kommer hertil")
-  //   if(Notification.permission === "granted") {
-  //     navigator.serviceWorker.getRegistration().then(function (reg) {
-  //       reg.showNotification("hej")
-  //     })
-  //   }
-  // }
-
   return (
     <>
-      <button onClick={() => {console.log("Det her virker")}}>Notification</button>
+      <button onClick={() => displayNotification("Du grim")}>Notification</button>
       <Searchbar state={setMovies}/>
       <Router>
         <Moviegrid default path="/" movies={movies}/>
