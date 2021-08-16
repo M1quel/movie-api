7860205eef5beaaed578a2a578393bcd1b665e2d
M1quel <5760459@rts-365.dk>
2021-08-16 12:21:40 +0200

    changes

--- src/components/helpers/displayNotification.js
+++ src/components/helpers/displayNotification.js
@@ -1,7 +1,9 @@
 export default function displayNotification (message) {
   if(Notification.permission === "granted") {
     navigator.serviceWorker.getRegistration().then(function (reg) {
-      reg.showNotification(message)
+      reg.showNotification(message, {
+        vibrate: [200, 100, 200, 100, 400]
+      })
     })
   }
 }
