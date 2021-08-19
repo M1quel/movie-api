0cb07f89eb94ba2ce78adc363b170af4c6c32d5b
Mikkel Frederiksen <5760459@rts-365.dk>
2021-08-19 23:27:43 +0200

    fixed refiredt from singleMovie on load

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Router } from '@reach/router';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.scss';
 import { navigate } from '@reach/router';
 import Moviegrid from './components/movieGrid/MovieGrid';
@@ -9,27 +9,32 @@ import fetchMovies from './components/helpers/fetch';
 
 
 function App() {
+  const isMounted = useRef(false)
   var [page, setPage] = useState(1);
   var [searchValue, setSearchValue] = useState("")
-  var [movies, setMovies] = useState([]);
+  var [movies, setMovies] = useState(undefined);
   var [totalMovies, setTotalMovies] = useState(0)
   useEffect(function () {
-    setMovies([])
-    var options = {
-      method: 'GET',
-      url: 'https://movie-database-imdb-alternative.p.rapidapi.com/',
-      params: {s: searchValue, page: `${page}`, r: 'json'},
-      headers: {
-        'x-rapidapi-key': 'cbf0eada93mshda4348a7166d51bp13e11bjsna5929dc3ff1a',
-        'x-rapidapi-host': 'movie-database-imdb-alternative.p.rapidapi.com'
-      }
-    };
-    navigate("/search")
-    fetchMovies(options).then((data) => {
-      setMovies(data.Search)
-      setTotalMovies(data.totalResults)
-      navigate("/")
-    })
+    if(isMounted.current) {
+      setMovies(undefined)
+      var options = {
+        method: 'GET',
+        url: 'https://movie-database-imdb-alternative.p.rapidapi.com/',
+        params: {s: searchValue, page: `${page}`, r: 'json'},
+        headers: {
+          'x-rapidapi-key': 'cbf0eada93mshda4348a7166d51bp13e11bjsna5929dc3ff1a',
+          'x-rapidapi-host': 'movie-database-imdb-alternative.p.rapidapi.com'
+        }
+      };
+      navigate("/search")
+      fetchMovies(options).then((data) => {
+        setMovies(data.Search)
+        setTotalMovies(data.totalResults)
+        navigate("/")
+      })
+    } else {
+      isMounted.current = true
+    }
   }, [page]) //eslint-disable-line
   // useEffect(function () {
   //   Notification.requestPermission(function(status) {
