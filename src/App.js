91f62239c9a4b5dc684516a7d4ac470066369ddc
Mikkel Frederiksen <5760459@rts-365.dk>
2021-08-19 15:45:03 +0200

    added rating system to database

--- src/App.js
+++ src/App.js
@@ -3,51 +3,10 @@ import { useEffect, useState } from 'react';
 import './App.scss';
 import Moviegrid from './components/movieGrid/MovieGrid';
 import Searchbar from "./components/searchbar/Searchbar";
-import displayNotification from './components/helpers/displayNotification';
 import Singlemovie from './components/singleMovie/Singlemovie';
 
 
 function App() {
-
-  // var [db, setDB] = useState(undefined)
-  // var request = window.indexedDB.open("test-db-47");
-
-  // request.onsuccess(function(event) {
-  //   console.log(event.target.result)
-  // })
-/*   var dbPromise = window.indexedDB.open('test-db1', 1, function(upgradeDB) {
-    //Making an object store
-    if (!upgradeDB.objectStoreNames.contains('rating')) {
-      var ratingDB = upgradeDB.createObjectStore('rating', {keyPath: "movieID"});
-      ratingDB.createIndex("movieID", "movieID", {unique: true});
-      ratingDB.createIndex("rating", "rating", {unique: false});
-    }
-
-  }) */
-  // useEffect(function () {
-  //   if (!('indexedDB' in window)) {
-  //     console.log('This browser doesn\'t support IndexedDB');
-  //     return;
-  //   }
-    
-  // }, [setDB])
-
-/*   useEffect(function () {
-    if(dbPromise) {
-      dbPromise().then(function(db) {
-        var tx = db.transaction('rating', 'readwrite');
-        var store = tx.objectStore('rating');
-        var item = {
-          movieID: "jiang",
-          rating: 3
-        };
-        store.add(item);
-        return tx.complete;
-      }).then(function() {
-        console.log('added item to the store os!');
-      });
-    }
-  }, [dbPromise]) */
 
   useEffect(function () {
     Notification.requestPermission(function(status) {
--- src/components/singleMovie/Singlemovie.js
+++ src/components/singleMovie/Singlemovie.js
@@ -1,35 +1,49 @@
 import axios from 'axios';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Spinner from '../Spinner';
 import "./Singlemovie.scss";
+import Localbase from 'localbase'
 export default function Singlemovie(props) {
 
     var [movie, setMovie] = useState(undefined) 
     var [userRating, setUserRating] = useState(undefined)
-
-    var starRef = useRef();
-    var ratingRef = useRef();
-    useEffect(function () {
-        if(!movie) return
-        var rating = ratingRef.current;
-        var ratingSelect = rating.querySelector(".ratingSelect")
-        ratingSelect.addEventListener("change", function () {
-            setRating(ratingSelect.value)
-        })
-    }, [movie])
-
-    function setRating (index) {
-        var starWrapper = starRef.current;
-        var stars = starWrapper.querySelectorAll("svg")
-        stars.forEach(star => {
-            star.querySelector("path").style.fill = "transparent"
-
-            if(star.getAttribute("index") <= index) {
-                star.querySelector("path").style.fill = "yellow"
+    
+    let db = new Localbase("db");
+    useEffect (function () {
+        db.collection("ratings").doc({ id: props.id }).get().then(document => {
+            if(document) {
+                setUserRating(document)
+                
+            } else {
+                return
             }
         })
+        
+    }, [])//eslint-disable-line
+
+    //Sætter den nye rating
+    function putUserRating (e) {
+        e.preventDefault();
+        var form = e.target;
+        db.collection("ratings").doc({ id: props.id }).add({
+            id: props.id,
+            rating: form.selectRating.value,
+            description: form.description.value
+        });
+        setUserRating({
+            id: props.id,
+            rating: form.selectRating.value,
+            description: form.description.value
+        })
+    }
+
+    //Fjerner rating fra filmen
+    function removeRating () {
+        db.collection("ratings").doc({ id: props.id }).delete();
+        setUserRating(undefined)
     } 
 
+    //Henter data om filmen
     useEffect(function () {
         var options = {
             method: 'GET',
@@ -73,24 +87,34 @@ export default function Singlemovie(props) {
                     </div>
                 </section>
 
-                <section className="rating">
-                    {"" ? <h1 className="rating__heading">Din tidligere rating</h1> : <h1 className="rating__heading">Giv en rating</h1>}
-                    <div ref={starRef} className="rating__wrapper">
-                        <svg index="1" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" class="svg-inline--fa fa-star fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>
-                        <svg index="2" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" class="svg-inline--fa fa-star fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>
-                        <svg index="3" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" class="svg-inline--fa fa-star fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>
-                        <svg index="4" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" class="svg-inline--fa fa-star fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>
-                        <svg index="5" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" class="svg-inline--fa fa-star fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>
-                    </div>
-                    <div ref={ratingRef}>
-                        <select className="ratingSelect" name="selectRating" id="selectRating">
-                            <option selected value="">Vælg Rating</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                        </select>
+                <section className="userRating">
+                    <h1 className="rating__heading">{userRating ? "Your rating" : "Give it a rating"}</h1>
+                    {userRating ? <div className="rating__wrapper">
+                        <svg index="1" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" className="svg-inline--fa fa-star fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill={userRating ? userRating.rating >= 1 ? "yellow" : "transparent" : "transparent"} d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>
+                        <svg index="2" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" className="svg-inline--fa fa-star fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill={userRating ? userRating.rating >= 2 ? "yellow" : "transparent" : "transparent"} d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>
+                        <svg index="3" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" className="svg-inline--fa fa-star fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill={userRating ? userRating.rating >= 3 ? "yellow" : "transparent" : "transparent"} d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>
+                        <svg index="4" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" className="svg-inline--fa fa-star fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill={userRating ? userRating.rating >= 4 ? "yellow" : "transparent" : "transparent"} d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>
+                        <svg index="5" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" className="svg-inline--fa fa-star fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill={userRating ? userRating.rating >= 5 ? "yellow" : "transparent" : "transparent"} d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>
+                    </div> : null}
+                    <div className="userRatingWrapper">
+                        {userRating ? <div className="userRating">
+                            <p className="userDescription__p">" {userRating.description ? userRating.description : ""} "</p>
+                            <button className="userRatingBTN" onClick={removeRating}>Remove Rating</button>
+                        </div> : 
+                        <form className="userRatingForm" onSubmit={(e) => {putUserRating(e);}}>
+                            {<select className="ratingSelect" name="selectRating" id="selectRating">
+                                <option defaultValue value="">Vælg Rating</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                            </select>}
+                            <textarea className="userDescription__textField" name="description" id="description" placeholder="Descripe your thoughts on the movie"></textarea>
+                            <button className="userRatingBTN" type="submit">Submit Rating</button>
+                        </form>}
+                        
+                        
                     </div>
                 </section>
             </div>
